26aaa1eaa782072a727269842e6c9e4f92d4eb0d
Hilalfadilah <hilalfadilah21@gmail.com>
2023-03-19 21:38:27 +0700

    fixed navbar,add responsive

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const bar = document.querySelector('.bar');
+const Navbar = document.querySelector('.navbar-nav');
 const sidebar = document.querySelector('.sidebar');
 
 sidebar.addEventListener('click', function () {
-    bar.classList.toggle('active')
+    Navbar.classList.toggle('active')
 });
 
 document.addEventListener('click', function (e) {
-    if (!bar.contains(e.target) && !sidebar.contains(e.target)) {
+    if (!Navbar.contains(e.target) && !sidebar.contains(e.target)) {
         bar.classList.remove('active');
     }
 });
